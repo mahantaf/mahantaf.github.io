2b4eaa8eec842c4cd7ccf44c15da8c420d41a570
mahantp <mahantp19@gmail.com>
2023-10-01 19:43:14 -0700

    ebay website added to the list

---
+++ ebay.com/ebay_files/verify.js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_1696210940485110({ImpressionID:"63bea3a7e847443893817ed64ee30fcd", ServerPublicDns:"rtbc-uw1.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "63bea3a7e847443893817ed64ee30fcd");}catch(e){}
+try{__verify_callback_1696210940485110({
+ResultID:2,
+Passback:"",
+AdWidth:728,
+AdHeight:90});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "63bea3a7e847443893817ed64ee30fcd");}catch(e){}
