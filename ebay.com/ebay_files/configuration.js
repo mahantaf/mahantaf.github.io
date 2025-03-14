93ded5b2148530e8b8840af57267a0498869474e
Mahan Tafreshipour <mahantp@dhcp-v006-097.mobile.uci.edu>
2025-03-13 22:07:57 -0700

    Forked From Minimal Mistakes Theem

--- ebay.com/ebay_files/configuration.js
+++
@@ -1,5 +0,0 @@
-window._automatic_tracking_config = {
-    dynamicSelectors: '#BottomPanel, #FootPanel .FootPanelInnr, .x-btf-river .d-vi-region, .merch-module .merch-placements-container, [id^="placement"] .recs-module .merch-placements-container',
-    clickElements: ['a', 'button', 'select', 'area', '#str_bkw_input', '#tracking-events-details-toggle-click', '#pass'],
-    maxClicks: 100
-};
